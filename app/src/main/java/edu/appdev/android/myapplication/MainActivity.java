1b62ec45c140fb66a0781b206b945ed2d0acc213
Papa Kojo Ampim-Darko <papakojoampimdarko>
2016-12-11 11:30:48 -0600

    AppDev 10/12

---
+++ app/src/main/java/edu/appdev/android/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.appdev.android.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void doThing(View v) {
+        Toast.makeText(getApplicationContext(), "RAhhhhh", Toast.LENGTH_LONG).show();
+    }
+}
